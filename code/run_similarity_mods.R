ce4397c681d99c29b864837e4120bdd5083dc34a
vboyce <vero.boyce@gmail.com>
2026-05-27 10:10:22 -0700

    add ordbeta sim

--- code/run_similarity_mods.R
+++ code/run_similarity_mods.R
@@ -15,24 +15,23 @@ options(
   brms.iter = 2000,
   brms.threads = threading(4)
 )
-
-p_beta_sim <- prior_string("normal(0,.1)", class = "b")
-p_sd_sim <- prior_string("normal(0,.1)", class = "sd")
-p_intercept_sim <- prior_string("normal(0.5,.2)", class = "Intercept")
+# Priors now on logit scale (ordbeta uses logit link)
+# logit(0.72) ≈ 0.94, so intercept around 1
+p_beta_sim <- prior_string("normal(0, 0.5)", class = "b")
+p_sd_sim <- prior_string("normal(0, 0.5)", class = "sd")
+p_intercept_sim <- prior_string("normal(1, 1.5)", class = "Intercept")
 
 sim_priors <- c(p_intercept_sim, p_beta_sim, p_sd_sim)
 
-sims_for_model <- read_rds(here("cached_model_files/data_for_mods/sims_for_model.rds")) |>
-  mutate(condition_id = as.factor(condition_id)) |>
-  filter(stage_num == 1)
-
 to_next_mod <- brm(sim ~ rep_num + (rep_num || dataset_id / condition_id),
+  family = ordbeta(), # <-- this line
   file = here("cached_model_files/mods/to_next_mod.rds"),
   prior = sim_priors,
   data = sims_for_model |> filter(sim_type == "to_next")
 )
 
 diverge_mod <- brm(sim ~ rep_num + (rep_num || dataset_id / condition_id),
+  family = ordbeta(),
   file = here("cached_model_files/mods/diverge_mod.rds"),
   prior = sim_priors,
   data = sims_for_model |> filter(sim_type == "diverge")
