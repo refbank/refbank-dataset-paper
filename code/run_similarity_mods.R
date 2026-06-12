2393a083d82eea41d2628b159bfe5954624da3ce
vboyce <vero.boyce@gmail.com>
2026-06-12 11:27:28 -0700

    add stage 1 only filter to sim analysis

--- code/run_similarity_mods.R
+++ code/run_similarity_mods.R
@@ -24,7 +24,7 @@ p_intercept_sim <- prior_string("normal(1, 1.5)", class = "Intercept")
 
 sim_priors <- c(p_intercept_sim, p_beta_sim, p_sd_sim)
 
-sims_for_model <- read_rds(here("cached_model_files/data_for_mods/sims_for_model.rds"))
+sims_for_model <- read_rds(here("cached_model_files/data_for_mods/sims_for_model.rds")) |> filter(stage_num == 1)
 
 to_next_mod <- ordbetareg(sim ~ rep_num + (rep_num || dataset_id / condition_id),
   manual_prior = sim_priors,
